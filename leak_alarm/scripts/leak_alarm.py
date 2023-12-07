164fcf7a680f7e86c9da3889653fd2df70e1ad4e
ignaciotb <ignaciotorroba@hotmail.com>
2023-12-07 10:31:56 +0100

    CM station

--- leak_alarm/scripts/leak_alarm.py
+++ leak_alarm/scripts/leak_alarm.py
@@ -3,23 +3,31 @@
 import rospy
 import os
 from sam_msgs.msg import Leak 
+from smarc_bt.msg import MissionControl
 
 class LeakAlarm(object):
 
     def pulse(self, event):
         rospy.loginfo("Checking for leaks %s", rospy.get_time())
 
-    def leak_cb(self, leak_msg):
-        if leak_msg.value:
-            r = rospy.Rate(1)
-            while not rospy.is_shutdown():
-                os.system('espeak -a 200 -p 99 -ven "Leak"')
-                r.sleep()
+    def fb(self, x):
+        return {
+            0 : 'RUNNING',
+            1 : 'STOPPED',
+            2 : 'PAUSED',
+            3 : 'EMERGENCY',
+            4 : 'RECEIVED',
+            5 : 'COMPLETED',
+        }[x]
+
+    def mission_control_cb(self, fb_msg):
+        word = self.fb(fb_msg.plan_state)
+        os.system('espeak -a 200 -p 99 -ven ' + word)
 
     def __init__(self):
 
-        self.leak_subs = rospy.Subscriber('/sam/core/leak_fb', Leak, self.leak_cb)
-        self.timer = rospy.Timer(rospy.Duration(5.), self.pulse)
+        self.mc_subs = rospy.Subscriber('/sam/smarc_bt/mission_control', MissionControl, self.mission_control_cb)
+        # self.timer = rospy.Timer(rospy.Duration(5.), self.pulse)
         
         rospy.spin()
 
